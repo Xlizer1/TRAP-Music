42811609fff03e2815ef6b3c0cc60a4f426f97a2
Mustafa Eltoum <57389533+Xlizer1@users.noreply.github.com>
2023-03-04 08:34:06 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
 
   prefix: "-", // required, command prefix
   botToken:
-    "MTA4MDkwNzA0NDY4NjY1OTYzNA.GIZF20.vMK9RPb1QlZcO6B1v2kjODOPOV7exm4kc8wFEw", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
-  clientId: "1080907044686659634", // optional, the bot's client/application ID, leave empty to disable slash commands
+    "", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+  clientId: "", // optional, the bot's client/application ID, leave empty to disable slash commands
   geniusApiToken:
-    "bpLB1BBm2dXgnADOhZwQFtp58Y54bXpWT-cLLUw_zZbcNXWwtLFdKg8NFVBEG1R6", // optional, but might be required for lyrics search, https://genius.com/api-clients
+    "", // optional, but might be required for lyrics search, https://genius.com/api-clients
 };
